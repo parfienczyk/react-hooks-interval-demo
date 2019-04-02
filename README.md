5e87813b88af81b1ae2318fdfe565edf7715af1b
Karol Parfienczyk <parfienczyk@gmail.com>
2019-04-02 10:26:11 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-hooks-interval-demo
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount(c => c + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="App">
+      <h2>count: {count}</h2>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
